d53568225ee7518d7276c6e7a5c86768ab977e40
Cypher <doccamarco@gmail.com>
2024-04-15 10:05:58 -0400

    In the middle of refactoring Example component.

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,19 @@ import { React, useState } from 'react';
 // import reactLogo from './assets/react.svg';
 // import viteLogo from '/vite.svg';
 import './App.css';
-import Header from './components/Header';
 import Example from './components/Example';
 import States from './components/States';
 
+
 function App() {
+
   const [count, setCount] = useState(0);
 
   return (
     <>
-      Hello World!
-      <Header/>
-      <Example/>
-      <States/>
+      <h1>Hello World!</h1>
+      <Example />
+      <States />
     </>
   );
 }

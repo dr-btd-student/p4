e75530f6e800ee9ffd63ce4213876bf91ae2694d
Cypher <doccamarco@gmail.com>
2024-04-15 16:53:51 -0400

    Implemented problem 3 solution

--- src/gettingStarted.jsx
+++ src/gettingStarted.jsx
@@ -3,9 +3,11 @@ import ReactDOM from "react-dom/client";
 // import App from "./App.jsx";
 import "./styles/main.css";
 import Example from "./components/Example/index.jsx";
+import Header from "./components/Header/index.jsx";
 
 ReactDOM.createRoot(document.getElementById("getting-started")).render(
 	<React.StrictMode>
+		<Header />
 		<Example />
 	</React.StrictMode>
 );
--- src/p2.jsx
+++ src/p2.jsx
@@ -3,9 +3,11 @@ import ReactDOM from "react-dom/client";
 // import App from "./App.jsx";
 import "./styles/main.css";
 import States from "./components/States/index.jsx";
+import Header from "./components/Header/index.jsx";
 
 ReactDOM.createRoot(document.getElementById("p2")).render(
 	<React.StrictMode>
+      <Header />
 		<States />
 	</React.StrictMode>
 );
